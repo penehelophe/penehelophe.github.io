44aca9c074298c8d2904eb0dedb18221af102c78
unknown <penehelophe@gmail.com>
2019-07-10 09:36:07 -0700

    this is my first commit

---
+++ home.js
@@ -0,0 +1,13 @@
+function alert_name() {
+  //create name variable
+  var name = "Penehelophe";
+  alert("My name is " + name)
+
+
+
+}
+
+function change_image() {
+  var image = document.getElementby.id('bts3.image')
+  image.src = "bts3.jpg"
+}
